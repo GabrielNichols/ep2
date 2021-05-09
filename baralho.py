9e8b9d2342fc8f02c1cef224133c3c628fbca6d3
EricPossato <ericandreilimapossato@gmail.com>
2021-05-09 11:23:51 -0300

    lista de movimentos

--- baralho.py
+++ baralho.py
@@ -8,15 +8,27 @@ def cria_baralho():
     return baralho
 
 def extrai_naipe(carta):
-
     return carta[len(carta)-1]
 
 def extrai_valor(carta):
-
     if carta[1] == '0':
-
         return '10'
-
     else:
-
         return carta[0]
+
+def lista_movimentos_possiveis(baralho, indice_carta):
+    resultado = []
+    carta = baralho[indice_carta]
+    if carta == baralho[0]:
+        return resultado
+    
+    #compara o valor e o naipe com carta anterior
+    if extrai_naipe(carta) == extrai_naipe(baralho[indice_carta-1]) or extrai_valor(carta) == extrai_valor(baralho[indice_carta-1]):
+        resultado.append(1)
+    
+    #compara valor e naipe com a terceira carta anteiror
+    if indice_carta > 2:
+        if extrai_naipe(carta) == extrai_naipe(baralho[indice_carta-3]) or extrai_valor(carta) == extrai_valor(baralho[indice_carta-3]):
+            resultado.append(3)
+    
+    return resultado
